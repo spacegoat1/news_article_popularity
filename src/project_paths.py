6d26b6ccf2ccac52c3ab33857c7ae8e118d88236
spacegoat1 <yash_b_mehta@brown.edu>
2021-10-05 09:41:05 -0400

    EDA WIP

--- src/project_paths.py
+++ src/project_paths.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import codecs
 import numpy as np
+import pickle
 
 root = os.environ["project_root"]
 print(root)
@@ -16,3 +17,13 @@ names_csv = raw_data_files_path + 'OnlineNewsPopularity.names'
 
 working_data_csv = path_data_interim + 'data.csv'
 features_list = path_data_interim + 'features_list.csv'
+
+def save_list_to_pkl(var, file):
+    with open(path_data_interim + file, 'wb') as f:
+        pickle.dump(var, f)
+
+def load_list_from_pkl(file):
+    with open(path_data_interim + file, 'rb') as f:
+        mynewlist = pickle.load(f)
+    return mynewlist
+
